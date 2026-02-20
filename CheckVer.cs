a8a856bf6e399c9a981fc5263bb40bf589899dc7
Steve Butski <coldmiser487@gmail.com>
2026-02-20 11:56:16 -0600

    Everything has been updated to version 0.26.138.493 and working correctly.  I thought I should do another commit before I break it again

--- CheckVer.cs
+++ CheckVer.cs
@@ -1,8 +1,22 @@
+using Serilog;
+using System.Reflection;
+
 namespace PatchInstaller
 {
-    /*
+
     public class CheckVersion()
     {
+        public void Run()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var version = assembly.GetName().Version?.ToString() ?? "n/a";
+
+            Log.Information("Version: {Version}", version);
+
+            // your other code here...
+        }
+
+    /*
         LogAssemblyInfo();
 
         Log.Information("Version: {Version}",
@@ -10,12 +24,12 @@ namespace PatchInstaller
         string tempPath = Path.Combine(Path.GetTempPath(), "myfile.txt");
 
         // With HttpClient (string)
-        string text = await client.GetStringAsync("https://example.com/file.txt");
+        string text = await client.GetStringAsync("https://raw.githubusercontent.com/Coldmiser/PatchInstaller/refs/heads/master/version.txt");
 //        File.WriteAllText(tempPath, text);
         Log.Information("DL-Version: {Version}",
             text);
 
         // your code here
-    }
     */
+    }
 }
--- run.bat
+++
@@ -1,2 +0,0 @@
-dotnet run
-dir "C:\Users\sbutski\Documents\VSCode Solutions Folder\PatchInstaller\bin\Debug\net10.0\PatchInstaller.exe"
